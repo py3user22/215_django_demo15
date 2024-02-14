d1f0d919e26c0f469a2f0ef040fd62fc71d968f9
JesusG <py3user22@github.com>
2024-02-14 13:36:50 -0600

    214 save1

---
+++ l_app215/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LApp215Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'l_app215'
---
+++ l_app215/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+import json
+from datetime import datetime, date
+
+from .forms import BookingForm
+from .models import Booking, Menu
+
+
+
+        # Create your views here.
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+
+
+@csrf_exempt
+def bookings(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        exist = Booking.objects.filter(reservation_date=data['reservation_date']).filter(
+            reservation_slot=data['reservation_slot']).exists()
+        if exist == False:
+            booking = Booking(
+                first_name=data['first_name'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot'],
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+
+    date = request.GET.get('date', datetime.today().date())
+
+    bookings = Booking.objects.all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+
+    return HttpResponse(booking_json, content_type='application/json')
+
+
+def home(request):
+    return render(request, 'index.html',{})
+
+def about(request):
+    return render(request, 'about.html')
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', {"menu": main_data})
+
+
+def display_menu_item(request, pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+    else:
+        menu_item = ""
+    return render(request, 'menu_item.html', {"menu_item": menu_item})
+
+
+#211 add demo view method to call the date
+
+def event_list(request):
+    reservations = Booking.objects.all()
+    return render(request, 'book.html', {'reservations':reservations})
+
